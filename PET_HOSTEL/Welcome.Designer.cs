c1ad471a0b77cb8aee97a037a557e3ba6d9d99de
nasir-sarkar <nasirsarkar537@gmail.com>
2025-02-03 02:14:54 +0600

    update-39

--- PET_HOSTEL/Welcome.Designer.cs
+++ PET_HOSTEL/Welcome.Designer.cs
@@ -283,10 +283,10 @@ namespace PET_HOSTEL
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(18, 58);
+            this.pictureBox1.Location = new System.Drawing.Point(27, 58);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(140, 137);
+            this.pictureBox1.Size = new System.Drawing.Size(131, 137);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
