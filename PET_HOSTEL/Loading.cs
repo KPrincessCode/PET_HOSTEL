ea990622d2d8facde65c5383fea6242035f1a356
nasir-sarkar <nasirsarkar537@gmail.com>
2025-02-02 22:29:48 +0600

    update-32

--- PET_HOSTEL/Loading.cs
+++ PET_HOSTEL/Loading.cs
@@ -26,7 +26,7 @@ namespace PET_HOSTEL
         {
             if (progressBar1.Value < 100)
             {
-                progressBar1.Value += 1;
+                progressBar1.Value += 5;
 
                 label2.Text = progressBar1.Value.ToString() + "%";
             }
